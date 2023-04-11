81e55adb391ba92a1cd63e00e955ea5b704bc9b9
Aleksandr Popov <123383522+Aleksandr210890@users.noreply.github.com>
2023-04-11 16:21:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(".korobka").click(function(){
+        $(".korobka").hide();
+    });
+    $(".korobka").click(function(){
+        $(".header__topp").show();
+      });
+    //   alert('pip');
+
+
+});
